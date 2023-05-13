416113df4a66acf8ab62b29fc3cdad057560ab1a
TiagoNunesFCT <tjf.nunes@campus.fct.unl.pt>
2023-05-13 23:40:56 +0100

    Seller Location System Added (precision.... 10m?)

--- lib/view/sellerLandingPage.dart
+++ lib/view/sellerLandingPage.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:camelvan_frontend/view/buyerLandingPage.dart';
 import 'package:camelvan_frontend/view/sellerMapPage.dart';
@@ -15,6 +17,16 @@ bool firstState = true;
 bool initializedMap = false;
 //Is this the first time geolocation data was aquired? (useful to automatically center and zoom for the first time)
 bool firstSignal = true;
+
+//seller coordinates
+double latitude = 0;
+double longitude = 0;
+
+
+
+
+
+
 
 //The Open Street Maps Tile Layer
 TileLayer mapService = TileLayer(
@@ -58,6 +70,115 @@ class SellerLandingPage extends StatefulWidget {
 }
 
 class _SellerLandingPageState extends State<SellerLandingPage> {
+
+  bool servicestatus = false;
+  bool haspermission = false;
+  late LocationPermission permission;
+  late Position position;
+  late StreamSubscription<Position> positionStream;
+
+
+
+
+  //The State's Initialization
+  @override
+  void initState() {
+
+;
+
+    //if this is the first time the page is running, initialize the map
+    if (firstState) {
+      /*initMap();*/
+    }
+
+    //start Geolocation
+    checkGps();
+    super.initState();
+
+
+
+
+
+  }
+
+
+
+  checkGps() async {
+    servicestatus = await Geolocator.isLocationServiceEnabled();
+    if(servicestatus){
+      permission = await Geolocator.checkPermission();
+
+      if (permission == LocationPermission.denied) {
+        permission = await Geolocator.requestPermission();
+        if (permission == LocationPermission.denied) {
+          print('Location permissions are denied');
+        }else if(permission == LocationPermission.deniedForever){
+          print("'Location permissions are permanently denied");
+        }else{
+          haspermission = true;
+        }
+      }else{
+        haspermission = true;
+      }
+
+      if(haspermission){
+        setState(() {
+          //refresh the UI
+        });
+
+        getLocation();
+      }
+    }else{
+      print("GPS Service is not enabled, turn on GPS location");
+    }
+
+    setState(() {
+      //refresh the UI
+    });
+  }
+
+  getLocation() async {
+    position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    print(position.longitude); //Output: 80.24599079
+    print(position.latitude); //Output: 29.6593457
+
+    longitude = position.longitude;
+    latitude = position.latitude;
+
+    setState(() {
+      //refresh UI
+    });
+
+    LocationSettings locationSettings = LocationSettings(
+      accuracy: LocationAccuracy.high, //accuracy of the location data
+      distanceFilter: 10, //minimum distance (measured in meters) a
+      //device must move horizontally before an update event is generated;
+    );
+
+    StreamSubscription<Position> positionStream = Geolocator.getPositionStream(
+        locationSettings: locationSettings).listen((Position position) {
+      print(position.longitude); //Output: 80.24599079
+      print(position.latitude); //Output: 29.6593457
+
+      longitude = position.longitude;
+      latitude = position.latitude;
+
+      setState(() {
+        //refresh UI on update
+      });
+    });
+  }
+
+
+
+
+
+
+
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -69,7 +190,8 @@ class _SellerLandingPageState extends State<SellerLandingPage> {
                 child: Stack(children:[FlutterMap(options: MapOptions(
                   center: LatLng(38.66, -9.17),
                   zoom: 13.0,
-                ), children: [mapService, mapMarkers],)/*,Image.asset('assets/bbicon.png', fit: BoxFit.scaleDown)*/])
+                ), children: [mapService, mapMarkers],)/*,Image.asset('assets/bbicon.png', fit: BoxFit.scaleDown)*/,
+                Text("Current Position: Lat: " + latitude.toString() + " Lon: " + longitude.toString())])
             )
         )
     );
@@ -91,3 +213,60 @@ class CachedTileProvider extends TileProvider {
     );
   }
 }
+
+
+
+/// Determine the current position of the device.
+///
+/// When the location services are not enabled or permissions
+/// are denied the `Future` will return an error.
+Future<Position> _determinePosition() async {
+  bool serviceEnabled;
+  LocationPermission permission;
+
+  // Test if location services are enabled.
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    // Location services are not enabled don't continue
+    // accessing the position and request users of the
+    // App to enable the location services.
+    return Future.error('Location services are disabled.');
+  }
+
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      // Permissions are denied, next time you could try
+      // requesting permissions again (this is also where
+      // Android's shouldShowRequestPermissionRationale
+      // returned true. According to Android guidelines
+      // your App should show an explanatory UI now.
+      return Future.error('Location permissions are denied');
+    }
+  }
+
+  if (permission == LocationPermission.deniedForever) {
+    // Permissions are denied forever, handle appropriately.
+    return Future.error(
+        'Location permissions are permanently denied, we cannot request permissions.');
+  }
+
+  // When we reach here, permissions are granted and we can
+  // continue accessing the position of the device.
+  return await Geolocator.getCurrentPosition();
+}
+
+enum _PositionItemType {
+  log,
+  position,
+}
+
+
+class _PositionItem {
+  _PositionItem(this.type, this.displayValue);
+
+  final _PositionItemType type;
+  final String displayValue;
+}
+
